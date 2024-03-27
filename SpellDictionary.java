6e56d2ec9c9b2112a842ee4c27308e5e2a8e4459
Isabelle Wang <ilwang@smith.edu>
2024-03-27 15:38:23 -0400

    trying deletion

--- SpellDictionary.java
+++ SpellDictionary.java
@@ -14,7 +14,6 @@ public class SpellDictionary implements SpellingOperations {
 
             File file = new File(fileName);
             Scanner reader = new Scanner(file);
-            
 
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
@@ -31,7 +30,7 @@ public class SpellDictionary implements SpellingOperations {
      * @return true if the query word is in the dictionary.
      */
     public boolean containsWord(String query) {
-        return dictionary.contains(query); 
+        return dictionary.contains(query);
     }
 
     /**
@@ -39,28 +38,66 @@ public class SpellDictionary implements SpellingOperations {
      * @return a list of all valid words that are one edit away from the query
      */
     public ArrayList<String> nearMisses(String query) {
-        ArrayList<String> newWords = new ArrayList<>(); 
+        ArrayList<String> newWords = new ArrayList<>();
 
-        //Deletion
-        for(int i = 0; i < query.length(); i++) {
-            String newDeletedWord = query.substring(i, query.length()); 
-            if(dictionary.contains(newDeletedWord)) {
-                newWords.add(newDeletedWord); 
+        ArrayList<String> queryList = new ArrayList<>();
+        for (int i = 0; i < query.length(); i++) {
+            queryList.add(query.substring(i, i + 1));
+        }
+
+        // Deletion
+        for (int i = 0; i < query.length(); i++) {
+            String deletedFirst = query.substring(i, query.length());
+            String deletedSecond = query.substring(i, i+1) + query.substring(i+2); 
+            System.out.println(deletedFirst + " " + deletedSecond);
+            if (dictionary.contains(deletedFirst)) {
+                newWords.add(deletedFirst);
+                System.out.println(newWords);
+            }
+            if(dictionary.contains(deletedSecond)) {
+                newWords.add(deletedSecond); 
             }
         }
 
-        //Insertions
-        for(int i = 0; i < 26*(query.length() + 1); i++) {
-            String addLetterWord = 
+        for(int i = 0; i < query.length() - 1; i++) {
+            for(int j = i + 1; j < query.length(); j++) {
+                
+
+            }
+        }
+
+        // Insertions
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            for (int i = 0; i < query.length() - 1; i++) {
+                String newWord = query.substring(i, i + 1) + String.valueOf(ch) + query.substring(i + 3);
+            }
+        }
+
+        // substitutiions
+        for (int i = 0; i < 25 * query.length(); i++) {
 
         }
 
-        for(int i = 0; i < 25 * n; i++) {
-            
+        // transposition
+        for (int i = 0; i < query.length() - 1; i++) {
+
         }
 
+        // Splits
+        for (int i = 0; i < query.length() - 1; i++) {
 
-        
+        }
+
+        return newWords;
+
+    }
+
+    public static void main(String[] args) {
+        // for (char ch = 'A'; ch <= 'Z'; ch++) {
+        // System.out.println(ch);
+        // }
+        SpellDictionary d = new SpellDictionary("words.txt");
+        d.nearMisses("hello");
 
     }
 
